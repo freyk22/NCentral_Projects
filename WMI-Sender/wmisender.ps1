0294ff532a8a625f9fe3e2300ed613580eb56d6d
freyk22 <fborgerink1@gmail.com>
2025-03-24 14:07:05 +0100

    updated wmi-sender

--- WMI-Sender/wmisender.ps1
+++ WMI-Sender/wmisender.ps1
@@ -1,120 +1,167 @@
 <#   
-== Description ===
-
-A powershell function that saves metrics from applications/devices to the wmi database.
-Metrics will be temporary saved in a array and the concent will be saved in the wmi database.
+WMI Sender
+Description: powershell Function to save data to a wmi/cim class 
 
 ## Under construction - use at your own risk #
 
-== Examples input ==
-#Example - adding data to collector
-ncentral_datatosend -item "Objectname4" -value "4444"
-#Example - create namespace
-fncCimClassCreate -namespace $namespace
-#update data in class
-Get-CIMInstance -Classname $servicename -Namespace $namespace | Where-Object Device_id -eq "$device_id" | Set-CimInstance -Property $datasendcollector_dic
+steps:
+1. change script settings
+2. add your script after function "ncentral_datatosend"
+2. add to following line, for the data you want to send: ncentral_datatosend -item "myitem" -value "1234"
+3. and end the script with the line: fncwmisender
 
-## Under construction- use at your own risk #
+#code example - adding/update data to collector
+ncentral_datatosend -item "Objectname4" -value "1234"
+fncwmisender;
 
 #info:
 https://documentation.n-able.com/N-central/userguide/Content/Services/Custom_Monitoring_Services/custom_mon_services_wmi.htm
-#https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-wmiinstance?view=powershell-5.1
-#https://www.reddit.com/r/PowerShell/comments/cmhtq8/wmi_sub_namespaces_syntax/?rdt=62918
- 
+https://github.com/N-able/CustomMonitoring
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-wmiinstance?view=powershell-5.1
+https://www.reddit.com/r/PowerShell/comments/cmhtq8/wmi_sub_namespaces_syntax/?rdt=62918
+
+## Under construction- use at your own risk #
+
 #> 
 
-function fncGetLogTimeStamp {return "[{0:yyyy/MM/dd} {0:HH:mm:ss}]" -f (Get-Date)} 
-function fnclog($logtext){Write-host "$(fncGetLogTimeStamp) $script_title - $logtext";} 
+### Settings ##
 $script_title = "wmisender"
+$Ncentral_device_customer = "mycustomer"
+$NCentral_device_id = "112"
+$NCentral_device_Name = "Myups"
+$NCentral_servicename = "PowerSupply"
+$Ncentral_wmi_namespace = "root\CIMV2\NCentral"
 
-$device_id = "112"
-$namespace = "root\CIMV2\NCentral\Device_$($device_id)"
-$servicename = "PowerSupply"
+#dont change things behind this line
+#dont change things behind this line
+#==================================
+#==================================
+#==================================
 
-fnclog "data-send collector - Preparing data-send collector"
-Set-Variable -Name datasendcollector_dic -Option AllScope
-$datasendcollector_dic = @{}
+function fncGetLogTimeStamp {return "[{0:yyyy/MM/dd} {0:HH:mm:ss}]" -f (Get-Date)} #end function fncGetLogTimeStamp
+
+function fnclog($logtext){Write-host "$(fncGetLogTimeStamp) $script_title - $logtext";} #end function fnclog
+
+function fncwmisender {
+    #function to create a namespace and the class
+    
+    function fncCimClassCreate {
+    #function to create a namespace, class, instance and add properties to it.
+        fnclog "Namespace creation - go"
+        fnclog "Namespace creation - creating namespace $namespace"
+        write-host "============================="
+
+        $subs = @($namespace -split '\\' | Select -Skip 1)
+        for ($i = 0; $i -lt $subs.Count; $i++)
+        {
+            $sub = $subs[$i]
+            write-host "creating class $sub"
+            $list = New-Object System.Collections.ArrayList
+            [void]$list.Add("root")
+            $list.AddRange(@($subs | Select -First $i))
+            $alreadyExists = $list -join '\'
+            $nsPath = "{0}:__NAMESPACE" -f $alreadyExists
+            # create the namespace
+            $ns = [wmiclass]$nsPath
+            $sc = $ns.CreateInstance()
+            $sc.Name = $sub
+            $sc.Put()
+            write-host "----"
+            #fnclog "Created namespace $sub under $alreadyExists" #-f Green
+        }
+        write-host "============================="
+        fnclog "Namespace creation - class created"
+    
+        #Creating class for device and add class properties to it
+        fnclog "class creation - setting class properties in $($namespace)\$NCentral_servicename"
+        $WMI_Class = New-Object System.Management.ManagementClass("$namespace", $null, $null)
+        $WMI_Class.name = "$NCentral_servicename"
+        $WMI_Class.Properties.Add("Devicename", [System.Management.CimType]::String, $false) 
+        $WMI_Class.Properties["Devicename"].Qualifiers.Add("key", $true) 
+        $WMI_Class.Properties.Add("Objectname",[System.Management.CimType]::String, $false) 
+        foreach ($datasendcollector_dic_key in $datasendcollector_dic.Keys) {
+                $WMI_Class.Properties.Add("$datasendcollector_dic_key",[System.Management.CimType]::String, $false) 
+        }
+        $WMI_Class.Put() 
+
+        #creating instance
+        fnclog "class creation - creating instance and adding data to it, in $($namespace)\$($NCentral_servicename)";
+        $instancetofill = Get-CimClass -Namespace $namespace -ClassName $NCentral_servicename
+        New-CimInstance -CimClass $instancetofill -Property $datasendcollector_dic #-ClientOnly
+        fnclog "Namespace creation - done"
+    }#end function fncCImclassCreate
+    
+
+    function fncInstance_Update{
+        #updating instance
+        fnclog "Update instance - go"
+        fnclog "Update instance - Update instance with data in instance $($namespace)\$($NCentral_servicename)";
+        $instancetoupdate = Get-CimClass -Namespace $($namespace) -ClassName $($NCentral_servicename) # | Where-Object "Device_id" -eq "$($NCentral_device_id)"
+        #Set-CimInstance -InputObject $instancetoset -Property @{"Devicename"="ups1";"Objectname"="MyObject2";}
+        #Get-CIMInstance -Classname $classname -Namespace $namespace | Where-Object Device_id -eq "$NCentral_device_id" | Set-CimInstance -Property @{Objectname3="3"}
+        Set-CimInstance -InputObject $instancetoupdate -Property $datasendcollector_dic
+        fnclog "Update instance - done"
+    }#end fncInstance_Update
+    
+    #Setting value of Namespace
+    $namespace = "$($Ncentral_wmi_namespace)\Device_$($NCentral_device_id)"
+
+    #Adding to static values to $datasendcollector_dic
+    $datasendcollector_dic = @{"Device_id" = $NCentral_device_id} + $datasendcollector_dic
+    $datasendcollector_dic = @{"Devicename" = $NCentral_device_Name} + $datasendcollector_dic
+    $datasendcollector_dic = @{"Device_customer" = $Ncentral_device_customer } + $datasendcollector_dic
+    
+    foreach ($datasendcollector_dic_key in $datasendcollector_dic.Keys) {
+        fnclog "Data-send collector - collectordata - $($datasendcollector_dic_key)=$($datasendcollector_dic[$datasendcollector_dic_key])"                                                                 
+    }
+
+    #check for namespace
+    fnclog "Namespace creation - creating namespace: $namespace"
+    fnclog "Namespace creation - checking if namespace excists"
+    $Simclassdetect = Get-CimClass -Namespace $namespace -ClassName $NCentral_servicename -ErrorAction Ignore
+    if($Simclassdetect){
+        fnclog "Class detection - class found";
+        fncInstance_Update
+    }else{
+        fnclog "Class dectection - class not found";
+        fncCimClassCreate;
+    }
+            
+    fnclog "datasender - done"
+
+}#end function fncNamespacecreate
 
 function ncentral_datatosend {
     #function to save data in dictionary
     param( [parameter(Mandatory=$true)] [string]$item,[string]$value) 
     fnclog "Data-send collector - adding to collector: $($item)=$($value)"
     $datasendcollector_dic.Add("$item", "$value")    
-}
+} #end function ncentral_datatosend
+
+#create data collector
+fnclog "data-send collector - Preparing data-send collector"
+Set-Variable -Name datasendcollector_dic -Option AllScope
+$datasendcollector_dic = @{}
+
+#==================================
+#==================================
+#==================================
+#dont change things above this line
+#dont change things above this line
+
+
+
+## Add your reader script here
+## Add your reader script here
+## Add your reader script here
+
+
+#Add data to send. (but deviceid, devicename en devicecostumer al already sended)
 fnclog "data-send collector - Adding Data"
-ncentral_datatosend -item "Device_id" -value "$device_id"
-ncentral_datatosend -item "Devicename" -value "ups1"
-ncentral_datatosend -item "Device_customer" -value "12345"
 ncentral_datatosend -item "Objectname" -value "12345"
 ncentral_datatosend -item "Objectname2" -value "2222"
 ncentral_datatosend -item "Objectname3" -value "3333"
 ncentral_datatosend -item "Objectname4" -value "4444"
 
-
-#display content of the collector
-foreach ($datasendcollector_dic_key in $datasendcollector_dic.Keys) {
-        fnclog "Data-send collector - collectordata - $($datasendcollector_dic_key)=$($datasendcollector_dic[$datasendcollector_dic_key])"                                                                 
-}
-
-function fncCimClassCreate {
-    #function to create a namespace and the class
-    param( [parameter(Mandatory=$true)] [string]$namespace)
-    fnclog "creating namespace $namespace - go"
-    write-host "============================="
-
-    $subs = @($namespace -split '\\' | Select -Skip 1)
-    for ($i = 0; $i -lt $subs.Count; $i++)
-    {
-        $sub = $subs[$i]
-        write-host "creating class $sub"
-        $list = New-Object System.Collections.ArrayList
-        [void]$list.Add("root")
-        $list.AddRange(@($subs | Select -First $i))
-        $alreadyExists = $list -join '\'
-        $nsPath = "{0}:__NAMESPACE" -f $alreadyExists
-        # create the namespace
-        $ns = [wmiclass]$nsPath
-        $sc = $ns.CreateInstance()
-        $sc.Name = $sub
-        $sc.Put()
-        write-host "----"
-        #fnclog "Created namespace $sub under $alreadyExists" #-f Green
-    }
-    write-host "============================="
-    fnclog "creating namespace $namespace - done"
-
-}#end function fncCimClassCreate
-fncCimClassCreate -namespace $namespace
-
-#Creating class for device and add class properties to it
-fnclog "creating class for service $servicename in $namespace" 
-fnclog "setting class properties in $($namespace)\$servicename"
-$WMI_Class = New-Object System.Management.ManagementClass("$namespace", $null, $null)
-$WMI_Class.name = "$servicename"
-$WMI_Class.Properties.Add("Devicename", [System.Management.CimType]::String, $false) 
-$WMI_Class.Properties["Devicename"].Qualifiers.Add("key", $true) 
-$WMI_Class.Properties.Add("Objectname",[System.Management.CimType]::String, $false) 
-foreach ($datasendcollector_dic_key in $datasendcollector_dic.Keys) {
-        $WMI_Class.Properties.Add("$datasendcollector_dic_key",[System.Management.CimType]::String, $false) 
-}
-$WMI_Class.Put() 
-
-#creating instance
-fnclog "creating instance and adding data to it, in $($namespace)\$($servicename)";
-$instancetofill = Get-CimClass -Namespace $namespace -ClassName $servicename
-New-CimInstance -CimClass $instancetofill -Property $datasendcollector_dic #-ClientOnly
-
-#updating instance
-fnclog "Update data in instance $namespace \ $servicename";
-$instanceset = Get-CimClass -Namespace $namespace -ClassName $servicename
-if($instanceset){
-        fnclog "update instance - instance found found";
-    }else{
-        fnclog "updating instance - instance not found found";
-}
-#Set-CimInstance -InputObject $instanceset -Property @{"Devicename"="ups1";"Objectname"="MyObject2";}
-#Set-CimInstance -InputObject $instanceset -Property $datasendcollector_dic
-#Get-CIMInstance -Classname $classname -Namespace $namespace | Where-Object Device_id -eq "$device_id" | Set-CimInstance -Property @{Objectname3="3"}
-Get-CIMInstance -Classname $servicename -Namespace $namespace | Where-Object Device_id -eq "$device_id" | Set-CimInstance -Property $datasendcollector_dic
-
-fnclog "done"
+#send all the data to the wmi sender
+fncwmisender #-namespace $namespace
