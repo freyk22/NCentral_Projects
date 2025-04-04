8833d77354c6b6e4a2985f7467c183a4fbbc0838
freyk22 <fborgerink1@gmail.com>
2025-04-04 13:25:14 +0200

    updated code

--- WMI-Sender/wmisender.ps1
+++ WMI-Sender/wmisender.ps1
@@ -8,17 +8,15 @@ steps:
 1. change script settings
 2. add your script after function "ncentral_datatosend"
 2. add to following line, for the data you want to send: ncentral_datatosend -item "myitem" -value "1234"
-3. and end the script with the line: fncwmisender
+3. and end the script with the line: fncncentralwmisender
 
 #code example - adding/update data to collector
 ncentral_datatosend -item "Objectname4" -value "1234"
-fncwmisender;
+fncncentralwmisender;
 
-#info:
+#Used code from:
 https://documentation.n-able.com/N-central/userguide/Content/Services/Custom_Monitoring_Services/custom_mon_services_wmi.htm
 https://github.com/N-able/CustomMonitoring
-https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-wmiinstance?view=powershell-5.1
-https://www.reddit.com/r/PowerShell/comments/cmhtq8/wmi_sub_namespaces_syntax/?rdt=62918
 
 ## Under construction- use at your own risk #
 
@@ -27,7 +25,7 @@ https://www.reddit.com/r/PowerShell/comments/cmhtq8/wmi_sub_namespaces_syntax/?r
 ### Settings ##
 $script_title = "wmisender"
 $Ncentral_device_customer = "mycustomer"
-$NCentral_device_id = "112"
+$NCentral_device_id = "118"
 $NCentral_device_Name = "Myups"
 $NCentral_servicename = "PowerSupply"
 $Ncentral_wmi_namespace = "root\CIMV2\NCentral"
@@ -42,7 +40,7 @@ function fncGetLogTimeStamp {return "[{0:yyyy/MM/dd} {0:HH:mm:ss}]" -f (Get-Date
 
 function fnclog($logtext){Write-host "$(fncGetLogTimeStamp) $script_title - $logtext";} #end function fnclog
 
-function fncwmisender {
+function fncncentralwmisender {
     #function to create a namespace and the class
     
     function fncCimClassCreate {
@@ -76,8 +74,10 @@ function fncwmisender {
         fnclog "class creation - setting class properties in $($namespace)\$NCentral_servicename"
         $WMI_Class = New-Object System.Management.ManagementClass("$namespace", $null, $null)
         $WMI_Class.name = "$NCentral_servicename"
+        $WMI_Class.Properties.Add("Device_id", [System.Management.CimType]::String, $false) 
+        $WMI_Class.Properties["Device_id"].Qualifiers.Add("key", $true) 
         $WMI_Class.Properties.Add("Devicename", [System.Management.CimType]::String, $false) 
-        $WMI_Class.Properties["Devicename"].Qualifiers.Add("key", $true) 
+        #$WMI_Class.Properties["Devicename"].Qualifiers.Add("key", $true) 
         $WMI_Class.Properties.Add("Objectname",[System.Management.CimType]::String, $false) 
         foreach ($datasendcollector_dic_key in $datasendcollector_dic.Keys) {
                 $WMI_Class.Properties.Add("$datasendcollector_dic_key",[System.Management.CimType]::String, $false) 
@@ -91,20 +91,17 @@ function fncwmisender {
         fnclog "Namespace creation - done"
     }#end function fncCImclassCreate
     
-
     function fncInstance_Update{
         #updating instance
         fnclog "Update instance - go"
-        fnclog "Update instance - Update instance with data in instance $($namespace)\$($NCentral_servicename)";
-        $instancetoupdate = Get-CimClass -Namespace $($namespace) -ClassName $($NCentral_servicename) # | Where-Object "Device_id" -eq "$($NCentral_device_id)"
-        #Set-CimInstance -InputObject $instancetoset -Property @{"Devicename"="ups1";"Objectname"="MyObject2";}
-        #Get-CIMInstance -Classname $classname -Namespace $namespace | Where-Object Device_id -eq "$NCentral_device_id" | Set-CimInstance -Property @{Objectname3="3"}
+        fnclog "Update instance - Update instance with data in instance $($namespace)"
+        $instancetoupdate = Get-CimInstance -Namespace $($namespace) -ClassName $($NCentral_servicename) | Where-Object "Device_id" -eq "$NCentral_device_id"
         Set-CimInstance -InputObject $instancetoupdate -Property $datasendcollector_dic
         fnclog "Update instance - done"
     }#end fncInstance_Update
     
     #Setting value of Namespace
-    $namespace = "$($Ncentral_wmi_namespace)\Device_$($NCentral_device_id)"
+    $namespace = "$($Ncentral_wmi_namespace)\$($NCentral_servicename)"
 
     #Adding to static values to $datasendcollector_dic
     $datasendcollector_dic = @{"Device_id" = $NCentral_device_id} + $datasendcollector_dic
@@ -118,15 +115,14 @@ function fncwmisender {
     #check for namespace
     fnclog "Namespace creation - creating namespace: $namespace"
     fnclog "Namespace creation - checking if namespace excists"
-    $Simclassdetect = Get-CimClass -Namespace $namespace -ClassName $NCentral_servicename -ErrorAction Ignore
+    $Simclassdetect = Get-CimInstance -Namespace $($namespace) -ClassName $($NCentral_servicename) -ErrorAction Ignore | Where-Object "Device_id" -eq "$NCentral_device_id" 
     if($Simclassdetect){
         fnclog "Class detection - class found";
-        fncInstance_Update
+        fncInstance_Update;
     }else{
         fnclog "Class dectection - class not found";
         fncCimClassCreate;
     }
-            
     fnclog "datasender - done"
 
 }#end function fncNamespacecreate
@@ -149,12 +145,9 @@ $datasendcollector_dic = @{}
 #dont change things above this line
 #dont change things above this line
 
-
-
 ## Add your reader script here
 ## Add your reader script here
 ## Add your reader script here
-
 
 #Add data to send. (but deviceid, devicename en devicecostumer al already sended)
 fnclog "data-send collector - Adding Data"
@@ -164,4 +157,4 @@ ncentral_datatosend -item "Objectname3" -value "3333"
 ncentral_datatosend -item "Objectname4" -value "4444"
 
 #send all the data to the wmi sender
-fncwmisender #-namespace $namespace
+fncncentralwmisender
